2d83d95e93d768da28b1d14dadd6eefa180935ff
tianmingjian <tianmingjian@work.com>
2024-09-20 15:35:09 +0800

    修改拼写错误

--- src/raw-body/raw-body.middleware.ts
+++ src/raw-body/raw-body.middleware.ts
@@ -4,7 +4,7 @@ import { RequestWithRawBody } from './raw-body.type';
 export const RawBodyMiddleware = (): ReturnType<typeof json> => {
   return json({
     verify: (request, response, buffer) => {
-      if (request.url?.startsWith('/webhook') && Buffer.isBuffer(buffer)) {
+      if (request.url?.startsWith('/web-hooks') && Buffer.isBuffer(buffer)) {
         (request as RequestWithRawBody).rawBody = Buffer.from(buffer);
       }
       return true;
